b71cc09c50f84ed7484f3b578d745bfad585c32b
Mohsen <Mohsen@DESKTOP-8AT1P8G>
2026-01-16 10:56:22 +0100

    ny commit (font och css)

--- Models/BoxResult.cs
+++ Models/BoxResult.cs
@@ -3,7 +3,7 @@
     // skapa en en egen värdetyp som tar in fördefinierade värden
     public enum BoxResult
     {
-        // fördefinierade värdena
+        // fördefinierade värdena till en box som innehåll
         Empty, Bomb, Gold
     }
 }
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,7 +3,6 @@ namespace Projekt_databas_och_w_system.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
